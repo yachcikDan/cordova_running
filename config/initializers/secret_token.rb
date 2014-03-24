687f66b897c8420e6910558f5ef10f33808247aa
Daniel Yachcik <dyachcik@gmail.com>
2014-03-24 16:20:36 -0500

    rails new cordova_running database postgresql --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CordovaRunning::Application.config.secret_key_base = '3f763342268ad59a1044c48052290044c586b4343394454da7bdbb3364682877fdb42ca15bbe1fa997cfff4ed245a96e4406c4eeac16e3b16a55557f78bbf38f'
